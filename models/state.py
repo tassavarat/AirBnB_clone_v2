61d50965c976d8144974a280fc1556d4bf657856
tassavarat <tassavarat@gmail.com>
2019-08-14 17:13:06 -0700

    Revert changes

--- models/state.py
+++ models/state.py
@@ -22,4 +22,4 @@ class State(BaseModel, Base):
             for data in self.cities:
                 if data.state_id == self.id:
                     my_list.append(data)
-            return(my_list)
+            return [cities for city in storage.all(models.City).values()]
