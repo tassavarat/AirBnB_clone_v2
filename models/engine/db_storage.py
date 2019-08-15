194676897bd9111426cd536de01d7b8b576d4ae6
tassavarat <tassavarat@gmail.com>
2019-08-15 00:28:52 +0000

    Removed unneeded variables

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -23,8 +23,7 @@ class DBStorage():
     def __init__(self):
         """Class constructor"""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(self.dialect, self.driver,
-                                             getenv('HBNB_MYSQL_USER'),
+                                      format(getenv('HBNB_MYSQL_USER'),
                                              getenv('HBNB_MYSQL_PWD'),
                                              getenv('HBNB_MYSQL_HOST'),
                                              getenv('HBNB_MYSQL_DB'),
