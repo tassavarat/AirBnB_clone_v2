b71ffcca9127fbdfde4265efdec0dd82d7858fb9
tassavarat <tassavarat@gmail.com>
2019-08-15 19:45:46 -0700

    Linter fixes

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -17,7 +17,6 @@ from models.engine.file_storage import FileStorage
 class TestDbFileStorage(unittest.TestCase):
     '''this will test the FileStorage'''
 
-
     def test_pep8_FileStorage(self):
         """Tests pep8 style"""
         style = pep8.StyleGuide(quiet=True)
@@ -26,7 +25,7 @@ class TestDbFileStorage(unittest.TestCase):
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "file", "Don't run file")
     def test_attr(self):
-	"""Checking attributes"""
+        """Checking attributes"""
     self.assertTrue(hasattr(DBStorage, "_DBStorage_engine"))
     self.assertTrue(hasattr(DBStorage, "_DBStorage_session"))
     self.assertTrue(hasattr(DBStorage, "all"))
