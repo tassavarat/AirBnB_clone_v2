5979c77b4346d00ff78ea27094a7e401d78d6a2c
tassavarat <tassavarat@gmail.com>
2019-08-15 17:44:41 -0700

    Linter fixes

--- models/place.py
+++ models/place.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-
 class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
@@ -35,15 +34,16 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
     place_amenity = Table("place_amenity", Base.metadata,
-                           Column('place_id', String(60),
-                                  ForeignKey('places.id'),
-                                  primary_key=True, nullable=False),
-                           Column('amenity_id', String(60),
-                                  ForeignKey('amenities.id'),
-                                  primary_key=True, nullable=False))
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-    	reviews = relationship("Review", cascade='all, delete', backref="place")
-    	amenities = relationship("Amenity", secondary='place_amenity',
+        reviews = relationship("Review", cascade='all, delete',
+                               backref="place")
+        amenities = relationship("Amenity", secondary='place_amenity',
                                  viewonly=False)
     else:
         @property
@@ -53,6 +53,7 @@ class Place(BaseModel, Base):
                 if data.place_id == self.id:
                     my_list.append(data)
             return(my_list)
+
         @property
         def amenities(self):
             my_list = []
@@ -60,6 +61,7 @@ class Place(BaseModel, Base):
                 if data.amenity_ids == self.id:
                     my_list.append(data)
             return(my_list)
+
         @amenities.setter
         def amenities(self, obj):
             if type(obj) == Amenity:
