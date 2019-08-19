2c6088d3b0d4629c13e646bb0858a203f1644b26
tassavarat <tassavarat@gmail.com>
2019-08-19 16:46:12 -0700

    Formatting changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,5 +15,5 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen \[::\]:80 default_server;/a location /hbnb_static/ {\nalias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+sed -i '/listen \[::\]:80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
