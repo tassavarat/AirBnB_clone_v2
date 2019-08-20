fcd22912cab34f8e88a4137e17d1b9ac7b89de48
tassavarat <tassavarat@gmail.com>
2019-08-19 23:10:04 -0700

    Changed conditional syntax

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -53,6 +53,6 @@ def deploy():
     Returns: Value of do_deploy, False if no archive created
     """
     archive_path = do_pack()
-    if not archive_path:
+    if archive_path is None:
         return False
     return do_deploy(archive_path)
