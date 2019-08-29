b8e74d89e9e1d4d01abc056b248804287ca8dcb0
tassavarat <tassavarat@gmail.com>
2019-08-29 16:24:10 -0700

    First commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""3-python_route module
+Starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return: Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return: HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Return: C text"""
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Return: Python text"""
+    return "Python %s" % text.replace('_', ' ')
+
+
+app.run(host='0.0.0.0')
