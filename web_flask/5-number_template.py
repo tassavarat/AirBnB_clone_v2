54edc87c45ae070d8e2ff7a2694a4c3abc28d8f2
tassavarat <tassavarat@gmail.com>
2019-08-29 16:46:19 -0700

    First commit

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""5-number_template module
+Starts a Flask web application"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return: Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return: HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Return: C text"""
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Return: Python text"""
+    return "Python %s" % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Return: n is a number if n is int"""
+    return "%s is a number" % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Return: HTML page only if n is integer"""
+    return render_template("5-number.html")
+
+
+app.run(host='0.0.0.0')
